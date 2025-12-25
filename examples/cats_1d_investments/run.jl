0a09d76f71cd847a1cf534c4bc5be0da8ac71149
Paul Serna-Torre <psernatorre@gmail.com>
2025-12-24 16:24:42 -0800

    add: Gurobi to project, run cats_1d_investments

--- examples/cats_1d_investments/run.jl
+++ examples/cats_1d_investments/run.jl
@@ -1,15 +1,15 @@
 # Use Julia package
 using Revise
 using Infiltrator
-using CATEX, MosekTools
+using CATEX, MosekTools, Gurobi
 
 
 # Set the main directory for the toy example
 main_dir = @__DIR__
 
 sys, mod = run_stocapex(;   main_dir = main_dir, 
-                            solver = Mosek.Optimizer, 
-                            solver_settings = Dict(),
+                            solver = Gurobi.Optimizer, 
+                            solver_settings = Dict("Crossover" => 0),
                             print_model = false,
                             model_settings = Dict(  "gen_costs" => "linear", 
                                                     "consider_shedding" => false)) 
