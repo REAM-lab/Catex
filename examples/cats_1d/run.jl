b8ec2ec6a9a1ca0c6cd0ae73c7eb3fd491a3c8e2
Paul Serna-Torre <psernatorre@gmail.com>
2026-01-07 19:34:43 -0800

    add: cats_1d scenario.

---
+++ examples/cats_1d/run.jl
@@ -0,0 +1,20 @@
+# Use Julia package
+using Revise
+using Infiltrator
+using CATEX, MosekTools
+
+
+# Set the main directory for the toy example
+main_dir = @__DIR__
+
+sys, mod = run_stocapex(;   main_dir = main_dir, 
+                            solver = Mosek.Optimizer, 
+                            solver_settings = Dict(),
+                            print_model = false,
+                            model_settings = Dict(  "gen_costs" => "linear", 
+                                                    "consider_shedding" => false)) 
+
+#sys = init_system(main_dir)
+
+println("Finished")
+
